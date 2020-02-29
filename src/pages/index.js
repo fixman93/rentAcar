d70d48a68113920c5d81f3e6be6df323266e4d9f
Boris Civcic <civcicboris93@gmail.com>
2020-02-29 08:10:27 +0100

    setup new project

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>test</div>
+        )
+    }
+}
+
+export default HomePage
